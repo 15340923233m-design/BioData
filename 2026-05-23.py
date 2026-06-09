338344b571dffaf62324aafbbcdee17f08316492
15340923233m-design <15340923233m@gmail.com>
2026-06-10 01:28:32 +0800

    Delete 2026-05-23.py

--- 2026-05-23.py
+++
@@ -1,9 +0,0 @@
-# 删掉了错误的 email.mime 导入
-import streamlit as st
-from PIL import Image
-
-st.title("Wang")
-
-# 用正确的 Image 类打开图片（大写 I）
-image = Image.open("OIP.png")
-st.image(image, caption="", width=500)
